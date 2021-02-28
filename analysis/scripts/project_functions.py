c3578f9b3322122b5de607d5d4ca6c07d54f3fec
Thuan Vo <thuan.vo@ubc.ca>
2021-02-28 02:13:57 -0800

    edit scripts for initial data cleaning

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,17 +1,15 @@
-from typing import Iterable, Type
 import pandas as pd
 import os
 
-def load_and_process_one(path=None):
-    if path is None:
+def load_and_process_one(source=None):
+    if source is None:
         raise TypeError("Expecting 1 argument but 0 argument is passed.")
-    elif not os.path.exists(path):
+    elif not os.path.exists(source):
         raise FileExistsError("Path does not exist.")
-    elif not os.path.isfile(path):
-        raise FileNotFoundError("No file found.")
+    elif os.path.isdir(source):
+        raise FileNotFoundError("Expected file path but diretory path is given.")
 
-    df = pd.read_csv(path, delimiter=',')
-    return df
+    return process(pd.read_csv(source, delimiter=','))
 
 
 def load_and_process_many(source=None):
@@ -28,9 +26,26 @@ def load_and_process_many(source=None):
             parts.append(
                 pd.read_csv(os.path.join(source,file), 
                 delimiter=","))
-    df = pd.concat(parts,axis=0,ignore_index=True)
-    return df
+    
+    return process(pd.concat(parts, axis=0, ignore_index=True))
+    
+
+
+def process(dataframe=None):    
+    if dataframe is None:
+        raise ValueError("Expecting 1 argument but 0 is passed.")
+    elif not isinstance(dataframe, pd.DataFrame):
+        raise TypeError(f"An oject of type DataFrame is expected but {type(dataframe)} is passed.")
+    
+    return  (dataframe
+                .drop(columns=['Id'])
+                .loc[dataframe["Year"] >= 1910]
+                .assign(Popular = (dataframe["Count"] > 1000))  
+                .reset_index(drop=True)
+            )
+
+    
 
 if __name__ == "__main__":
-    df = load_and_process_one(path='../../data/raw/NationalNames.csv')
+    df = load_and_process_one('data/raw/national/NationalNames.csv')
     print(df)
